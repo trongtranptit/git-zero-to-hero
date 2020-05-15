0b5de2e8b64ea6c99e7229e033a4b2dd8303246b
TrongTran <giatran2311@gmail.com>
2020-05-16 03:43:52 +0700

    update cat class

--- Cat.js
+++ Cat.js
@@ -1,4 +1,10 @@
 function Cat(name, age) {
     this.name = name;
     this.age = age;
+    this.stomach = []
 }
+
+Cat.prototype.eat = function(mouse){
+    this.stomach.push(mouse);
+}
+module.exports = Cat;
