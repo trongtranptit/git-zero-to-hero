4f341acd66133baa07013f1ea79aed5ce6d7230f
TrongTran <giatran2311@gmail.com>
2020-05-16 11:10:32 +0700

    new dog

---
+++ Dog.js
@@ -0,0 +1,9 @@
+function Dog(name, age){
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+}
+Dog.prototype.eat = function(cat){
+    this.stomach.push(cat);
+}
+module.exports = Dog;
