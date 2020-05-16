2b81e3fdc19b05084f429fd308f68ba53874eba3
TrongTran <giatran2311@gmail.com>
2020-05-16 18:41:26 +0700

    revert cat class

--- Cat.js
+++ Cat.js
@@ -1,11 +1,4 @@
 function Cat(name, age, weight) {
     this.name = name;
     this.age = age;
-    this.stomach = [];
-    this.weight = weight
 }
-
-Cat.prototype.eat = function(mouse){
-    this.stomach.push(mouse);
-}
-module.exports = Cat;
